26b2acbfb4b5da8ea1887121701b67ea08751666
kOstovic <ostacro.jug2@gmail.com>
2018-05-21 16:23:44 +0200

    working hyperledger nodes added

--- hyperledger/index.js
+++ hyperledger/index.js
@@ -1,29 +1,28 @@
 const http = require('http');
-const port = 3001 //FER 3001, FSB 3002, FFZG 3003
 var fs = require('fs'); //a
 const rp = require('request-promise')
-const AdminConnection = require('composer-admin').AdminConnection;
+//const AdminConnection = require('composer-admin').AdminConnection;
 const BusinessNetworkConnection = require('composer-client').BusinessNetworkConnection;
 const { BusinessNetworkDefinition, CertificateUtil, IdCard } = require('composer-common');
 const filename = "inputsTest.csv";// inputsTest.csv tid,jmbag - only 1 member; inputs.csv tid,jmbag; inputsTime.csv tid,jmbag,ttime
-const _universityKey = "0036"; //0036 FER, 0035 FSB, 1111 FFZG
-const method = "CheckAccessFER"; //CheckAccessFER, CheckAccessFSB, CheckAccessFFZG
-const cardName = "admin@pii-szg-network"; // fer@pii-szg-network , fsb@pii-szg-network, ffzg@pii-szg-network
+var port; 
+var _universityKey; 
+var method; 
+var cardName;
+bizNetworkConnection =  new BusinessNetworkConnection();
+bizNetworkConnection.on('event', (event) => {
+    console.log("Access "+ event.memberAccessBool+" for "+event.memberAccess.member.$identifier+" at "+_universityKey+" in "+event.timestamp+"\n");
+});
 
-async function getRandomLine(filename){
-  var _line = '';
-  fs.readFile(filename, function(err, data){
-    if(err) throw err;
-    var lines = data.toString().split('\n');
-    _line = lines[Math.floor(Math.random()*lines.length)];
-    //console.log(_line);
-    return _line;
+//promise to get random number of the line
+async function getRandomLine(lines) {
+  return new Promise((resolve, reject) => {
+    resolve(Math.floor(Math.random()*(lines.length -1)));
   });
-  //console.log(_line);
-  return _line;
 }
 
-function stringFromArray(data)
+//transform byte JMBAG to string ascii jmbag
+async function stringFromArray(data)
 {
   var count = data.length;
   var str = "";
@@ -34,53 +33,83 @@ function stringFromArray(data)
 }
 
 module.exports = {
-  serverStart: async function(){
-  /*  
-  }
-};*/
-//async function serverStart() {
-
-  const requestHandler = (request, response) => {
-    console.log(request.url)
-    response.end('Hello Node.js Server!')
-  }
-  
-  const server = http.createServer(requestHandler);
-  await server.listen(port, (err) => {
-    if (err) {
-      return console.log('something bad happened', err)
+  serverStart: async function(config){//config
+    switch(config){
+      case "FER":  port = 3001 //FER 3001, FSB 3002, FFZG 3003
+                  _universityKey = "0036"; //0036 FER, 0035 FSB, 1111 FFZG
+                  method = "CheckAccessFER"; //CheckAccessFER, CheckAccessFSB, CheckAccessFFZG
+                  cardName = "admin@pii-szg-network"; // fer@pii-szg-network , fsb@pii-szg-network, ffzg@pii-szg-network
+                  businessNetworkDefinition = await bizNetworkConnection.connect(cardName);
+                  break;
+      case "FSB":  port = 3002 //FER 3001, FSB 3002, FFZG 3003
+                  _universityKey = "0035"; //0036 FER, 0035 FSB, 1111 FFZG
+                  method = "CheckAccessFSB"; //CheckAccessFER, CheckAccessFSB, CheckAccessFFZG
+                  cardName = "fsb@pii-szg-network"; // fer@pii-szg-network , fsb@pii-szg-network, ffzg@pii-szg-network
+                  businessNetworkDefinition = await bizNetworkConnection.connect(cardName);
+                  break;
+      case "FFZG": port = 3003 //FER 3001, FSB 3002, FFZG 3003
+                  _universityKey = "1111"; //0036 FER, 0035 FSB, 1111 FFZG
+                  method = "CheckAccessFFZG"; //CheckAccessFER, CheckAccessFSB, CheckAccessFFZG
+                  cardName = "admin@pii-szg-network"; // fer@pii-szg-network , fsb@pii-szg-network, ffzg@pii-szg-network
+                  businessNetworkDefinition = await bizNetworkConnection.connect(cardName);
+                  break;
+      default: port = 3001 //FER 3001, FSB 3002, FFZG 3003
+              _universityKey = "0036"; //0036 FER, 0035 FSB, 1111 FFZG
+              method = "CheckAccessFER"; //CheckAccessFER, CheckAccessFSB, CheckAccessFFZG
+              cardName = "admin@pii-szg-network"; // fer@pii-szg-network , fsb@pii-szg-network, ffzg@pii-szg-network
+              businessNetworkDefinition = await bizNetworkConnection.connect(cardName);
+              break;
     }
     
-    console.log(`server is listening on ${port}`);
-    //while(1) { 
+    const requestHandler = (request, response) => {
+      console.log(request.url)
+      response.end('Hello Node.js Server!')
+    }
+    
+    const server = http.createServer(requestHandler);
+    await server.listen(port, (err) => {
+      if (err) {
+        return console.log('something bad happened', err)
+      }
+      
+      
+      console.log(`server is listening on ${port}`);
+      
+      //reapeat every 10 sec
       setInterval(async function () {
-      await fs.readFile(filename, async function(err, data){
-        if(err) throw err;
-        var lines = data.toString().split('\n');
-        let _line = await lines[Math.floor(Math.random()*lines.length)];
-        console.log("Sending this line from inputs.csv to chaincode "+_line);
-        let _lines = _line.split(',');
-        let _tid = _lines[0];
-        let _jmbag = stringFromArray(_lines[1]);
-        console.log(_tid+ " "+ _jmbag);
+        await fs.readFile(filename, async function(err, data){
+          if(err) { 
+            console.log(err); 
+            throw err;
+          }
+          
+          //reading line from file and extracting information
+          var lines =  await data.toString().split('\n');
+          _lineNum = await getRandomLine(lines);
+          let _line = lines[_lineNum];
+          console.log("Sending this line from inputs.csv to chaincode "+_line);
+          let _rows =  _line.split(',');
+          let _tid =  _rows[0];
+          let _jmbag = await stringFromArray(_rows[1]);
+          
+          //forming new transaction
+          
+          let factory = businessNetworkDefinition.getFactory();
+          let transaction    = factory.newTransaction('org.szg', method);
+          transaction.universityComponent  = factory.newRelationship('org.szg', 'UniversityComponent', _universityKey);
+          transaction.member = factory.newRelationship('org.szg', 'Member', _jmbag);
+          transaction.tid = _tid;
+          
+          //sending transaction to chaincode and catching event from that transaction 
+          
+
         
-        bizNetworkConnection =  new BusinessNetworkConnection();
-        businessNetworkDefinition = await bizNetworkConnection.connect(cardName);
-        let factory = businessNetworkDefinition.getFactory();
-        
-        let transaction    = factory.newTransaction('org.szg', method);
-        transaction.universityComponent  = factory.newRelationship('org.szg', 'UniversityComponent', _universityKey);
-        transaction.member = factory.newRelationship('org.szg', 'Member', _jmbag);
-        transaction.tid = _tid;
-        bizNetworkConnection.on('event', (event) => {
-          console.log("Access "+ event.memberAccessBool+" for "+_jmbag+" at "+_universityKey+" in "+event.timestamp);
-        });
-        await bizNetworkConnection.submitTransaction(transaction);
-      });  
-    }, 10000);
-    //} //
-  })
+          await bizNetworkConnection.submitTransaction(transaction);
+          
+        });  
+      }, 10000);
+    })
+  }
 }
-}
-//serverStart();
+
 require('make-runnable');
